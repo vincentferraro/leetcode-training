944102dec30b124521075e58f55c7d39b28434a4
vincentferraro <v.ferraro@live.fr>
2023-10-19 16:59:03 -0400

    add SQL 50

--- SQL/SQL50.sql
+++ SQL/SQL50.sql
@@ -12,5 +12,9 @@ SELECT DISTINCT(author_id) as id FROM Views WHERE author_id = viewer_id ORDER BY
 
 # 1378 REPLACE EMPLOYEE ID WITH THE UNIQUE IDENTIFIER
 SELECT EmployeeUNI.unique_id as unique_id , Employees.name as name FROM Employees LEFT JOIN EmployeeUNI ON EmployeeUNI.id=Employees.id;
+
+#1607 CUSTOMER WHO VISITED BUT DID NOT MAKE ANY TRANSACTIONS
+SELECT customer_id, COUNT(customer_id) as count_no_trans  FROM visits v LEFT JOIN Transactions t ON v.visit_id=t.visit_id WHERE transaction_id IS NULL GROUP BY customer_id ORDER BY COUNT(customer_id) DESC;
+
 # 1757
 SELECT product_id FROM Products WHERE low_fats='Y' AND recyclable='Y';
