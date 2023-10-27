db7b39439919d696bbc39321edafe32514da8226
vincentferraro <v.ferraro@live.fr>
2023-10-27 08:37:59 -0400

    67.AddBinary

---
+++ algorithm/67.AddBinary.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    let val1= a.split('')
+    let val2 = b.split()
+    let i1= val1.length-1
+    let i2 = val2.length-1
+    let addOne = false
+    let res
+    let lengthArr = val1.length <= val2.length ? val1.length -1: val2.length-1
+     while(i1 !== 0 || i2 !== 0){
+         if(parseInt(val1[i1])+parseInt(val2[i2]=== 2){
+             res = res +1
+         }else if(parseInt(val1[i1])+parseInt(val2[i2]=== 2){
+
+         }else{
+             
+         }
+     }
+    console.log(val1)
+};
---
+++ main.js
@@ -0,0 +1,6 @@
+let str = "100"
+
+str = "3" + str
+
+console.log(str)
+// console.log(str.slice(0,-1))
