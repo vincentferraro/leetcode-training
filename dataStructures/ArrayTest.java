4efdb8c7909b59d834afa23a23c1ed312145ffad
vincentferraro <v.ferraro@live.fr>
2024-02-14 20:25:05 -0500

    array

---
+++ dataStructures/ArrayTest.java
@@ -0,0 +1,19 @@
+package dataStructures;
+import java.util.Arrays;
+
+class ArrayTest {
+    
+    public static void main(String[] args) {
+        int [] arr = {3,2,5,4};
+
+        for(int a : arr){
+            System.out.println(a);
+        }
+        System.out.println();
+        Arrays.sort(arr);
+        for(int a : arr){
+            System.out.println(a);
+        }
+    }
+}
+
